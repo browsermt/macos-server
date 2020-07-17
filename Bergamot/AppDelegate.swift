f1aadc571165dfefb3f788ce692b4a13e32561cb
kdavis-mozilla <kdavis@mozilla.com>
2020-07-17 16:09:33 +0200

    Initial macOS Menu Bar App that runs the "say" command line tool

--- Bergamot/AppDelegate.swift
+++ Bergamot/AppDelegate.swift
@@ -11,16 +11,44 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    let serverProcess = Process()
+    let commandLineArguments = ["Started server"]
+    let executableURL = URL(fileURLWithPath: "/usr/bin/say")
+    
+    let statusItem = NSStatusBar.system.statusItem(withLength:NSStatusItem.squareLength)
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        if let button = statusItem.button {
+          button.image = NSImage(named:NSImage.Name("StatusBarButtonImage"))
+          button.action = #selector(startServer(_:))
+        }
+        constructMenu()
+        constructProcess()
+    }
+    
+    func constructMenu() {
+        let menu = NSMenu()
+
+        menu.addItem(NSMenuItem(title: "Start Bergamot Server", action: #selector(AppDelegate.startServer(_:)), keyEquivalent: "S"))
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem(title: "Quit Bergamot", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
+
+        statusItem.menu = menu
+    }
+    
+    func constructProcess() {
+        serverProcess.executableURL = executableURL
+        serverProcess.arguments = commandLineArguments
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+        serverProcess.terminate()
     }
-
-
+    
+    @objc func startServer(_ sender: Any?) {
+        if false == serverProcess.isRunning {
+            try! serverProcess.run()
+        }
+    }
 }
 
