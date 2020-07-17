33dd213d5ca3858b66d29139ed791d5aa8912a82
kdavis-mozilla <kdavis@mozilla.com>
2020-07-17 11:28:24 +0200

    Initial Commit

---
+++ Bergamot/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Bergamot
+//
+//  Created by Kelly Davis on 17.07.20.
+//  Copyright © 2020 Mozilla. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Bergamot/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Bergamot
+//
+//  Created by Kelly Davis on 17.07.20.
+//  Copyright © 2020 Mozilla. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
